d410dfcc6985605dcd179bf644d4e1162fca6506
Alan Coomes <acoomes1@cord.edu>
2020-07-31 17:06:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(int_arr)
+  int_arr.sort
+end
+
+def sort_array_desc(int_arr)
+  int_arr.sort do |a, b|
+    b<=>a
+  end
+end
+
+def sort_array_char_count(str_arr)
+  str_arr.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(int_arr)
+  int_arr.reverse
+end
+
+def kesha_maker
+  
+end
+
+def find_a
+  
+end
+
+def sum_array(int_arr)
+  int_arr.inject(:+)
+end
+
+def add_s
+  
+end
+
 
